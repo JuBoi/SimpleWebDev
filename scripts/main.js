debd6f4a0f385f25c1192707c9ec908a1426dbc7
Bryan Ju <bryanju@Bryans-MacBook-Pro.local>
2018-07-29 13:05:59 -0500

    stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 
 var essayButton = document.getElementById('essay');
-var last = document.querySelector('#potato');
 var modal =document.getElementById('Modal');
 var span = document.getElementsByClassName("close")[0];
+var next = document.getElementById('next');
+var back = document.getElementById('back');
+back.style.background = "#8D8282";
+var dataDiv = document.getElementById('dataDiv')
 
 essayButton.onclick = function() {
 	modal.style.display = "block";
 }
 
-span.onClick = function() {
+span.onclick = function() {
 	modal.style.display = "none";
 }
 
@@ -17,3 +20,57 @@ window.onclick = function(event) {
 		modal.style.display = "none";
 	}
 }
+
+next.onclick = function() {
+	removeStuff();
+	firstChart();
+}
+
+function removeStuff() {
+	back.style.background = "#4CAF50";
+	back.disabled = false;
+	$('#heading').remove();
+	$('#pokeball').remove();
+	$('#first').remove();
+	$('#second').remove();
+
+	essayButton.style.visibility = "hidden";
+}
+
+function firstChart() {
+	d3.select("body").append("svg").attr("width", 960).attr("height", 600);
+	var svg = d3.select("svg");
+	width = +svg.attr("width");
+	height = +svg.attr("height");
+	radius = Math.min(width, height) / 2;
+	g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+	var pie = d3.pie().sort(null).value(function(d) { return d.population; });
+
+	var path = d3.arc().outerRadius(radius - 10).innerRadius(0);
+
+	var label = d3.arc().outerRadius(radius - 40).innerRadius(radius - 40);
+
+	d3.csv("data/data.csv", function(d) {
+	  	d.population = +d.population;
+	  	return d;
+	}, function(error, data) {
+  	if (error) throw error;
+
+  	var arc = g.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc");
+
+  	arc.append("path").attr("d", path).attr("fill", function(d) { return color(d.data.age); });
+
+  	arc.append("text")
+      .attr("transform", function(d) { return "translate(" + label.centroid(d) + ")"; })
+      .attr("dy", "0.35em")
+      .text(function(d) { return d.data.age; });
+});
+
+}
+
+
+
+
